dc5fb993c1453390b4d82ecd112b79fa933507eb
Shanilka <shanilka799@gmail.com>
2023-11-13 22:27:36 +0530

    first commit

---
+++ src/main/java/lk/ijse/semisterfinal/Tm/CashierTm.java
@@ -0,0 +1,21 @@
+package lk.ijse.semisterfinal.Tm;
+
+import javafx.scene.control.Button;
+import lombok.*;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+@Getter
+@Setter
+
+public class CashierTm {
+
+    private String code;
+    private String description;
+    private int qty;
+    private double unitPrice;
+    private double tot;
+    private Button btn;
+
+}
--- src/main/java/lk/ijse/semisterfinal/controller/DiliverController.java
+++ src/main/java/lk/ijse/semisterfinal/controller/DiliverController.java
@@ -1,4 +1,31 @@
 package lk.ijse.semisterfinal.controller;
 
+import com.jfoenix.controls.JFXButton;
+import javafx.event.ActionEvent;
+import javafx.scene.control.Label;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
+
 public class DiliverController {
+    public AnchorPane root;
+    public TextField txtItemId;
+    public TextField txtdiscount;
+    public TextField txtQty;
+    public JFXButton btnAddToBill;
+    public Label lblNetTotal;
+    public Label lbitemName;
+    public Label lbitemPrice;
+    public TableColumn colItemId;
+    public TableColumn colItemName;
+    public TableColumn colUnitPrice;
+    public TableColumn colQty;
+    public TableColumn colDiscount;
+    public TableColumn colTotal;
+
+    public void btnAddToBillOnAction(ActionEvent event) {
+    }
+
+    public void btnPayOnAction(ActionEvent event) {
+    }
 }
