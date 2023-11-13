2a6abbc626ce63c659dac3870e2ab39d31841278
Shanilka <shanilka799@gmail.com>
2023-11-14 04:08:56 +0530

    first commit

--- src/main/java/lk/ijse/semisterfinal/Launcher.java
+++ src/main/java/lk/ijse/semisterfinal/Launcher.java
@@ -9,12 +9,11 @@ import javafx.stage.Stage;
 public class Launcher extends Application {
     public static void main(String[] args) {
         launch(args);
-
     }
 
     @Override
     public void start(Stage stage) throws Exception {
-        stage.setScene(new Scene(FXMLLoader.load(getClass().getResource("/view/EmployeeDhashborad.fxml"))));
+        stage.setScene(new Scene(FXMLLoader.load(getClass().getResource("/view/adminMainDashbord.fxml"))));
         stage.centerOnScreen();
         stage.setTitle("DashBoard");
 
--- src/main/java/lk/ijse/semisterfinal/controller/AddSupplierControlller.java
+++ src/main/java/lk/ijse/semisterfinal/controller/AddSupplierControlller.java
@@ -1,4 +1,13 @@
 package lk.ijse.semisterfinal.controller;
 
+import javafx.event.ActionEvent;
+import javafx.scene.control.TextField;
+
 public class AddSupplierControlller {
+    public TextField txtSupName;
+    public TextField txtSupId;
+    public TextField txtsupItemName;
+
+    public void addSupplierOnAction(ActionEvent event) {
+    }
 }
