94e2c13d392c352295f8b1a590e2974a83ac2abb
Shanilka <shanilka799@gmail.com>
2023-11-15 23:46:39 +0530

    first commit

--- src/main/java/lk/ijse/semisterfinal/controller/EmployeeController.java
+++ src/main/java/lk/ijse/semisterfinal/controller/EmployeeController.java
@@ -2,6 +2,7 @@ package lk.ijse.semisterfinal.controller;
 
 import javafx.event.ActionEvent;
 import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
 
 public class EmployeeController {
@@ -14,17 +15,25 @@ public class EmployeeController {
     public TextField txtAddress;
     public TextField txtStsrtDate;
     public TextField txtPossition;
-    public TableColumn tmid;
+    public TableColumn <?, ?>  tmid;
+    public TableColumn <?, ?>  tmEmpName;
+    public TableColumn <?, ?>  tmEmpGender;
+    public TableColumn <?, ?>  tmEmpMobile;
+    public TableColumn <?, ?>  tmEmpAddress;
+    public TableColumn <?, ?>  tmStartDate;
+    public TableColumn <?, ?>  tmEmpPossition;
+    public TableView < ? >  EmployeeTm;
 
     public void EmployeeAddOnAction(ActionEvent event) {
+
     }
 
     public void EmployeeUpdateOnAction(ActionEvent event) {
+
     }
 
     public void EmployeeDeleteOnAction(ActionEvent event) {
+
     }
 
-    public void EmployeeClearOnAction(ActionEvent event) {
-    }
 }
