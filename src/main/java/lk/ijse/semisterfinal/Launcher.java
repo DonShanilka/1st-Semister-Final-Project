50d0e5b904dd1b2ccbe716a547ebad077f942f64
Shanilka <shanilka799@gmail.com>
2023-11-17 14:06:29 +0530

    first commit

--- src/main/java/lk/ijse/semisterfinal/Launcher.java
+++ src/main/java/lk/ijse/semisterfinal/Launcher.java
@@ -5,7 +5,6 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
-
 public class Launcher extends Application {
     public static void main(String[] args) {
         launch(args);
