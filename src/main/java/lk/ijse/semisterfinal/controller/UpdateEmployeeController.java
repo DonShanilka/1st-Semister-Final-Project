6daaa544328fb61e144ec564ad0f21eab6025deb
Shanilka <shanilka799@gmail.com>
2023-11-22 20:34:55 +0530

    first commit

--- src/main/java/lk/ijse/semisterfinal/controller/UpdateEmployeeController.java
+++ src/main/java/lk/ijse/semisterfinal/controller/UpdateEmployeeController.java
@@ -1,5 +1,16 @@
 package lk.ijse.semisterfinal.controller;
 
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.TextField;
+
 public class UpdateEmployeeController {
 
+    public ComboBox <?> updateEmpId;
+    public TextField updateEmpaddress;
+    public TextField updateEmpName;
+    public TextField updateEmpMobile;
+    public DatePicker updateEmpDate;
+    public ComboBox updateEmpGender;
+    public TextField updateEmpPosition;
 }
