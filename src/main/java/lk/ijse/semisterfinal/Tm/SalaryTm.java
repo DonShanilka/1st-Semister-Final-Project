5617360a3aa56e8851dfe5cf040197b13f4faaac
Shanilka <shanilka799@gmail.com>
2023-11-25 12:43:51 +0530

    first commit

---
+++ src/main/java/lk/ijse/semisterfinal/Tm/SalaryTm.java
@@ -0,0 +1,5 @@
+package lk.ijse.semisterfinal.Tm;
+
+public class SalaryTm {
+
+}
--- src/main/java/lk/ijse/semisterfinal/dto/SalaryDTO.java
+++ src/main/java/lk/ijse/semisterfinal/dto/SalaryDTO.java
@@ -1,4 +1,12 @@
 package lk.ijse.semisterfinal.dto;
 
+import java.util.Date;
+
 public class SalaryDTO {
+
+    private String employeeId;
+    private String employeeName;
+    private double salary;
+    private String date;
+
 }
