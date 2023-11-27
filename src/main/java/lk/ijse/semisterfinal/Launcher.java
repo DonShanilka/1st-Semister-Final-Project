bd944e301c17916a14866205cd1400039d3eb934
Shanilka <shanilka799@gmail.com>
2023-11-27 16:24:45 +0530

    first commit

--- src/main/java/lk/ijse/semisterfinal/Launcher.java
+++ src/main/java/lk/ijse/semisterfinal/Launcher.java
@@ -12,7 +12,7 @@ public class Launcher extends Application {
 
     @Override
     public void start(Stage stage) throws Exception {
-        stage.setScene(new Scene(FXMLLoader.load(getClass().getResource("/view/Attendance.fxml"))));
+        stage.setScene(new Scene(FXMLLoader.load(getClass().getResource("/view/AddItem.fxml"))));
         stage.centerOnScreen();
         stage.setTitle("DashBoard");
 
