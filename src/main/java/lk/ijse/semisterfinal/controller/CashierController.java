5f0a1ba349e5fa159eb33ad0f98147ebedb41ff4
Shanilka <shanilka799@gmail.com>
2023-11-28 23:28:00 +0530

    first commit

--- src/main/java/lk/ijse/semisterfinal/controller/CashierController.java
+++ src/main/java/lk/ijse/semisterfinal/controller/CashierController.java
@@ -214,6 +214,7 @@ public class CashierController {
         double unitPrice = Double.parseDouble(lblUnitPrice.getText());
         int  qty = Integer.parseInt(txtQty.getText());
         double tot = (double)qty * unitPrice;
+        double discount;
         Button btn = new Button("Remove");
 
         setRemoveBtnAction(btn);
